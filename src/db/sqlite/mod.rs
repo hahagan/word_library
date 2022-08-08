580a11d45c97b62132afbf2ad30a5275cffe70cf
root <root@LAPTOP-S37HBQC8.localdomain>
2022-08-08 21:27:31 +0800

    [feat](sqlite) add database word operator

--- src/db/sqlite/mod.rs
+++ src/db/sqlite/mod.rs
@@ -1,5 +1,10 @@
+
+use sqlite::{Connection, Error};
 use std::fmt;
-use sqlite::{Connection, Statement, Type, Value};
+// use crate::store::RCError as RCStoreError;
+use crate::store::{Error as StoreError};
+
+
 
 use crate::store::{Store, World};
 const LIBNAME:&str = "library";
@@ -10,60 +15,59 @@ const CREATE_TABLE:&str = r#"CREATE TABLE  IF NOT EXISTS library (
 
 extern crate sqlite;
 
-type Error = Box<dyn std::error::Error>;
+type _Error = InternalError<StoreError<Error>>;
+pub type Result<T> = std::result::Result<T, _Error>;
 
-struct Sqlite {
-  connect: sqlite::Connection
+#[derive(Debug)]
+pub enum InternalError<T>{
+    NotFound,
+    Unknow(T),
 }
 
+impl<T:std::fmt::Debug> std::error::Error for InternalError<T> {}
+impl<T> fmt::Display for InternalError<T> {
+    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result{
+        write!(f, "internal error")
+    }
+}
+
+
+impl From<Error> for _Error{
+  fn from(err: Error) -> Self {
+    InternalError::Unknow(StoreError { message: String::from("unKnow"), code: 0, err: Some(err) })
+}
+}
+
+struct Sqlite {
+  connect: sqlite::Connection,
+  _insert_stmt: String,
+  _delete_stmt: String,
+  _update_stmt: String,
+  _get_stmt: String,
+}
+
+
 impl Sqlite {
-  fn create_table(&self)  -> Result<(),Error> {
+  fn create_table(&self)  -> Result<()> {
     let con = &self.connect;
     con.execute(CREATE_TABLE)?;
     Ok(())
   }
 }
 
-
-impl  Store<World,Error> for Sqlite {
-  fn insert(&self, word: World) -> Result<World,Error> {
-    let con = &self.connect;
-    let mut stmt = con.prepare("INSTER INTO {} (word, message) VALUES ({}, {})")?
-    .bind(0, LIBNAME)?
-    .bind(1, word.name.as_str())?
-    .bind(2, word.message.as_str())?;
-
-    stmt.next()?;
-
-    Ok(word)
-  }
-  fn update(&self, word: World) ->  Result<World,Error> {
-    return Ok(word);
-
-  }
-  fn delete(&self, word: &str) ->  Result<World,Error>{
-    Ok(World{
-      name: word.to_string(),
-      message: LIBNAME.to_string(),
-    })
-  }
-  fn get(&self, id: &str) -> Result<World,Error> {
-    return Ok(World{
-      name: id.to_string(),
-      message: LIBNAME.to_string(),
-    });
-
-  }
-}
-
 impl Sqlite {
-  pub fn new(path:String) -> Result<Sqlite, Box<dyn std::error::Error>> {
+
+  pub fn new(path:String) -> Result<Sqlite> {
     
     let con = Connection::open_with_flags(path, 
       sqlite::OpenFlags::new().set_no_mutex().set_read_write().set_create())?;
 
     let s = Sqlite{
-      connect: con
+      connect: con,
+      _insert_stmt: format!("INSERT INTO {} (word, message) VALUES (?, ?);", LIBNAME),
+      _delete_stmt: format!("DELETE FROM {} WHERE word=?", LIBNAME),
+      _update_stmt: format!("UPDATE {} SET message=? WHERE word=?;", LIBNAME),
+      _get_stmt: format!("SELECT message FROM {} WHERE word=?", LIBNAME)
     };
     s.create_table()?;
 
@@ -71,14 +75,93 @@ impl Sqlite {
   }
 }
 
+
+// lazy_static!{
+//   static ref init:i32 = {
+//     unsafe {
+//       NOT_FOUND_ERROR = Some(StoreError::new(0, String::from("notfound"))); 
+//     }
+//     1
+//   };
+// }
+
+// static NOT_FOUND_ERROR:_Error = StoreError::newInternal(0);
+
+impl Store<(), _Error> for Sqlite {
+  fn insert(&self, word: &World) -> Result<()> {
+    let con = &self.connect;
+    let mut stmt = con.prepare(self._insert_stmt.as_str())?
+    // .bind(0, LIBNAME)?
+    .bind(1, word.name.as_str())?
+    .bind(2, word.message.as_str())?;
+    stmt.next()?;
+
+    Ok(())
+  }
+  fn update(&self, word: &World) ->  Result<()> {
+    let mut stmt = self.connect.prepare(self._update_stmt.as_str())?
+    .bind(1, word.message.as_str())?
+    .bind(2, word.name.as_str())?;
+    stmt.next()?;
+    
+    Ok(())
+
+  }
+  fn delete(&self, word: &str) ->  Result<()>{
+    let con = &self.connect;
+    let mut stmt = con.prepare(self._delete_stmt.as_str())?
+    // .bind(1, LIBNAME)?
+    .bind(1, word)?;
+    stmt.next()?;
+    Ok(())
+  }
+  fn get(&self, id: &str) -> Result<World> {
+    let mut stmt = self.connect.prepare(self._get_stmt.as_str())?
+    .bind(1, id)?;
+
+    let state = stmt.next()?;
+    match state {
+        sqlite::State::Done => {
+          // Err(RCStoreError::new(1, String::from("qwe")))
+          Err(InternalError::NotFound)
+        }
+        sqlite::State::Row => {
+          Ok(World{
+            name: id.to_string(),
+            message: stmt.read(0)?,
+          })
+        }
+    }
+
+  }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let sql = Sqlite::new(String::from("qwe")).unwrap();
-        println!("{}", sql.connect.change_count())
+        let sql = Sqlite::new(String::from("target/test.sql")).unwrap();
+        let mut word = World { name: String::from("test"), message: String::from("test") };
+
+        sql.insert(&word).unwrap();
+        word.message = String::from("test update");
+        sql.update(&word).unwrap();
+        let word1 = sql.get(&word.name).unwrap();
+        assert_eq!(word.message, word1.message);
+        sql.delete(&word.name).unwrap();
+        
+        let err = sql.get("nofound").expect_err("notfound errir");
+        match  err {
+            InternalError::NotFound => {},
+            InternalError::Unknow(unknow_err) => {
+              panic!("{}",unknow_err)
+            }
+        }
+
         // assert_eq!(sql, None);
         // assert_ne!(sql, None);
     }
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,14 +1,32 @@
-use std::string;
+use std::{fmt};
 
-
+#[derive(Debug)]
 pub struct World {
     pub name: String,
     pub message: String
 }
 
 pub trait Store<T,E> {
-    fn insert(&self, word: World) -> Result<T,E>;
-    fn update(&self, word: World) -> Result<T,E>;
+    fn insert(&self, word: &World) -> Result<T,E>;
+    fn update(&self, word: &World) -> Result<T,E>;
     fn delete(&self, word: &str) -> Result<T,E>;
     fn get(&self, id: &str) -> Result<World,E>;
 }
+
+
+#[derive(Debug)]
+pub struct Error<T>{
+    pub message: String,
+    pub code: i32,
+    pub err: Option<T>
+}
+
+impl<T:std::fmt::Debug> std::error::Error for Error<T> {}
+impl<T> fmt::Display for Error<T> {
+    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result{
+        write!(f, "{}", self.message)
+    }
+}
+
+
+
