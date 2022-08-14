821358ef04c8217affd4fdaf98ac30afb72af62a
root <root@LAPTOP-S37HBQC8.localdomain>
2022-08-14 12:51:27 +0800

    [feat] add back end for word libray

---
+++ src/db/sqlite/store.rs
@@ -0,0 +1,296 @@
+use sqlite::Connection;
+
+use crate::store::{Read, Store, Transaction as StoreTransaction, World, Write};
+const LIBNAME: &str = "library";
+const CREATE_TABLE: &str = r#"CREATE TABLE  IF NOT EXISTS library (
+      word TEXT PRIMARY KEY,
+      message TEXT NOT NULL
+    )"#;
+
+use super::*;
+
+extern crate sqlite;
+
+pub struct Sqlite {
+    _path: String,
+    connect: sqlite::Connection,
+    _insert_stmt: String,
+    _delete_stmt: String,
+    _update_stmt: String,
+    _get_stmt: String,
+    _dump_stmt: String,
+    _dump_size_stmt: String,
+}
+
+impl Sqlite {
+    fn create_table(&self) -> Result<()> {
+        let con = &self.connect;
+        con.execute(CREATE_TABLE)?;
+        Ok(())
+    }
+
+    pub fn new(path: String) -> Result<Sqlite> {
+        let s = Sqlite::new_conn(path)?;
+        s.create_table()?;
+        Ok(s)
+    }
+
+    fn new_conn(path: String) -> Result<Sqlite> {
+        let con = Connection::open_with_flags(
+            &path,
+            sqlite::OpenFlags::new()
+                .set_full_mutex()
+                .set_read_write()
+                .set_create(),
+        )?;
+
+        let s = Sqlite {
+            connect: con,
+            _path: path,
+            _insert_stmt: format!("INSERT INTO {} (word, message) VALUES (?, ?);", LIBNAME),
+            _delete_stmt: format!("DELETE FROM {} WHERE word=?", LIBNAME),
+            _update_stmt: format!("UPDATE {} SET message=? WHERE word=?;", LIBNAME),
+            _get_stmt: format!("SELECT message FROM {} WHERE word=?", LIBNAME),
+            _dump_stmt: format!("SELECT word, message from {}", LIBNAME),
+            _dump_size_stmt: format!("SELECT word, message from {} LIMIT ?", LIBNAME),
+        };
+        Ok(s)
+    }
+}
+
+impl Write<World, InternalError<Error>> for Sqlite {
+    fn insert(&self, word: &World) -> Result<Option<World>> {
+        let con = &self.connect;
+        let mut stmt = con
+            .prepare(self._insert_stmt.as_str())?
+            // .bind(0, LIBNAME)?
+            .bind(1, word.name.as_str())?
+            .bind(2, word.message.as_str())?;
+        stmt.next()?;
+
+        Ok(None)
+    }
+    fn update(&self, word: &World) -> Result<Option<World>> {
+        let mut stmt = self
+            .connect
+            .prepare(self._update_stmt.as_str())?
+            .bind(1, word.message.as_str())?
+            .bind(2, word.name.as_str())?;
+        stmt.next()?;
+
+        Ok(None)
+    }
+    fn delete(&self, word: &str) -> Result<Option<World>> {
+        let con = &self.connect;
+        let mut stmt = con
+            .prepare(self._delete_stmt.as_str())?
+            // .bind(1, LIBNAME)?
+            .bind(1, word)?;
+        stmt.next()?;
+        Ok(None)
+    }
+}
+
+impl Read<World, InternalError<Error>> for Sqlite {
+    fn get(&self, id: &str) -> Result<World> {
+        let mut stmt = self.connect.prepare(self._get_stmt.as_str())?.bind(1, id)?;
+
+        let state = stmt.next()?;
+        match state {
+            sqlite::State::Done => {
+                // Err(RCStoreError::new(1, String::from("qwe")))
+                Err(InternalError::NotFound)
+            }
+            sqlite::State::Row => Ok(World {
+                name: id.to_string(),
+                message: stmt.read(0)?,
+            }),
+        }
+    }
+
+    // WARN memory head may be very big
+    // TODO add a iterator for batch select sql
+    fn list(&self, size: i64) -> Result<Vec<World>> {
+        let mut stmt: sqlite::Statement;
+        if size <= 0 {
+            stmt = self.connect.prepare(self._dump_stmt.as_str())?
+        } else {
+            stmt = self
+                .connect
+                .prepare(self._dump_size_stmt.as_str())?
+                .bind(1, size)?;
+        }
+
+        let mut res: Vec<World> = Vec::new();
+        loop {
+            match stmt.next()? {
+                sqlite::State::Done => {
+                    break;
+                }
+                sqlite::State::Row => res.push(World {
+                    name: stmt.read(0)?,
+                    message: stmt.read(1)?,
+                }),
+            }
+        }
+
+        Ok(res)
+    }
+}
+
+impl Store<World, InternalError<Error>, Tansaction> for Sqlite {
+    fn begin(&self) -> Result<Tansaction> {
+        let s = Sqlite::new_conn(String::from(self._path.as_str()))?;
+        {
+            let con = &s.connect;
+            let mut state = con.prepare("BEGIN")?;
+            state.next()?;
+        }
+
+        Ok(Tansaction { stmt: s })
+    }
+}
+
+pub struct Tansaction {
+    // pub stmt: &'a super::store::Sqlite,
+    pub stmt: Sqlite,
+}
+
+impl Read<World, InternalError<Error>> for Tansaction {
+    fn get(&self, id: &str) -> Result<World> {
+        self.stmt.get(id)
+    }
+
+    // WARN memory head may be very big
+    // TODO add a iterator for batch select sql
+    fn list(&self, size: i64) -> Result<Vec<World>> {
+        self.stmt.list(size)
+    }
+}
+
+impl<'a> Write<World, InternalError<Error>> for Tansaction {
+    fn insert(&self, word: &World) -> Result<Option<World>> {
+        self.stmt.insert(word)
+    }
+
+    fn update(&self, word: &World) -> Result<Option<World>> {
+        self.stmt.update(word)
+    }
+
+    fn delete(&self, word: &str) -> Result<Option<World>> {
+        self.stmt.delete(word)
+    }
+}
+
+impl<'a> StoreTransaction<World, InternalError<Error>> for Tansaction {
+    fn rollback(self) -> std::result::Result<(), InternalError<Error>> {
+        self.stmt.connect.execute("ROLLBACK")?;
+        Ok(())
+    }
+
+    fn commit(self) -> std::result::Result<(), InternalError<Error>> {
+        self.stmt.connect.execute("COMMIT")?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Debug;
+
+    use super::*;
+
+    fn new_sqlite() -> Sqlite {
+        Sqlite::new(String::from("target/test.sql")).unwrap()
+    }
+
+    fn clean_sqlite() {
+        std::fs::remove_file("target/test.sql").unwrap();
+    }
+
+    fn expect_notfound<T: Debug>(err: Result<T>) {
+        match err.expect_err("no error happen") {
+            InternalError::NotFound => {}
+            err => {
+                panic!("{}", err)
+            }
+        }
+    }
+
+    fn expect_unknow<T: Debug>(err: Result<T>) -> Error {
+        match err.expect_err("no error happen") {
+            InternalError::Unknow(err) => err.err.unwrap(),
+            err => {
+                panic!("shuold input a unkonw error, {}", err)
+            }
+        }
+    }
+
+    #[test]
+    fn transaction() {
+        let sql = new_sqlite();
+        let word = World {
+            name: String::from("test"),
+            message: String::from("test"),
+        };
+        sql.insert(&word).unwrap();
+
+        // rollback
+        let tran = sql.begin().unwrap();
+        tran.delete(&word.message).unwrap();
+        expect_notfound(tran.stmt.get(&word.name));
+        tran.rollback().unwrap();
+        sql.get(&word.name).unwrap();
+
+        //commit
+        let tran = sql.begin().unwrap();
+        tran.delete(&word.name).unwrap();
+        expect_notfound(tran.stmt.get(&word.name));
+        tran.commit().unwrap();
+        expect_notfound(sql.get(&word.name));
+
+        clean_sqlite();
+    }
+
+    #[test]
+    fn store() {
+        let sql = new_sqlite();
+        let mut word = World {
+            name: String::from("test"),
+            message: String::from("test"),
+        };
+
+        sql.insert(&word).unwrap();
+
+        // primary key conflict error
+        let err = expect_unknow(sql.insert(&word));
+        assert_eq!(err.code.unwrap() as i32, sqlite3_sys::SQLITE_CONSTRAINT);
+
+        word.message = String::from("test update");
+        sql.update(&word).unwrap();
+
+        // test get
+        let mut word1 = sql.get(&word.name).unwrap();
+        assert_eq!(word.message, word1.message);
+
+        // test list
+        word1.name = String::from("test1");
+        sql.insert(&word1).unwrap();
+
+        let ws = sql.list(0).unwrap();
+        assert_eq!(ws.len(), 2);
+
+        let ws = sql.list(1).unwrap();
+        assert_eq!(ws.len(), 1);
+
+        // test delete
+        sql.delete(&word.name).unwrap();
+
+        // test get not found
+        let err = sql.get("nofound").expect_err("notfound errir");
+        assert!(matches!(err, InternalError::NotFound));
+
+        // clean
+        clean_sqlite();
+    }
+}
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,32 +1,199 @@
-use std::{fmt};
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug)]
 pub struct World {
     pub name: String,
-    pub message: String
+    pub message: String,
 }
 
-pub trait Store<T,E> {
-    fn insert(&self, word: &World) -> Result<T,E>;
-    fn update(&self, word: &World) -> Result<T,E>;
-    fn delete(&self, word: &str) -> Result<T,E>;
-    fn get(&self, id: &str) -> Result<World,E>;
+pub trait Transaction<T, E>: Write<T, E> + Read<T, E> {
+    fn rollback(self) -> Result<(), E>;
+    fn commit(self) -> Result<(), E>;
 }
 
+pub trait Write<T, E> {
+    fn insert(&self, word: &World) -> Result<Option<T>, E>;
+    fn update(&self, word: &World) -> Result<Option<T>, E>;
+    fn delete(&self, word: &str) -> Result<Option<T>, E>;
+}
+
+pub trait Read<T, E> {
+    fn get(&self, id: &str) -> Result<World, E>;
+    fn list(&self, size: i64) -> Result<Vec<World>, E>;
+}
+pub trait Store<T, E, F: Transaction<T, E>>: Write<T, E> + Read<T, E> {
+    fn begin(&self) -> Result<F, E>;
+}
 
 #[derive(Debug)]
-pub struct Error<T>{
+pub struct Error<E> {
     pub message: String,
     pub code: i32,
-    pub err: Option<T>
+    pub err: Option<E>,
 }
 
-impl<T:std::fmt::Debug> std::error::Error for Error<T> {}
+impl<T: std::fmt::Debug> std::error::Error for Error<T> {}
 impl<T> fmt::Display for Error<T> {
-    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)
     }
 }
 
+#[derive(Debug)]
+pub enum InternalError<E> {
+    NotFound,
+    StoreNotFound,
+    Unknow(Error<E>),
+}
 
+impl<E: std::fmt::Debug> std::error::Error for InternalError<E> {}
+impl<E> fmt::Display for InternalError<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "internal error")
+    }
+}
 
+// pub struct WordLibrary<T, E, F: Transaction<T, InternalError<E>>, S: Store<T, InternalError<E>, F>>
+pub struct WordLibrary<T, E, F>
+where
+    F: Transaction<T, InternalError<E>>,
+    // S: Store<T, InternalError<E>, F>,
+{
+    stores: HashMap<String, Box<dyn Store<T, InternalError<E>, F>>>,
+    // stores: HashMap<String, S>,
+}
+
+impl<
+        T,
+        E: std::fmt::Debug,
+        F: Transaction<T, InternalError<E>>,
+        // S: Store<T, InternalError<E>, F>,
+    > WordLibrary<T, E, F>
+{
+    pub fn insert(&self, word: &World, key: &str) -> Result<Option<T>, InternalError<E>> {
+        let s = self.get_store(key)?;
+        s.insert(word)
+    }
+
+    pub fn update(&self, word: &World, key: &str) -> Result<Option<T>, InternalError<E>> {
+        let s = self.get_store(key)?;
+        s.update(word)
+    }
+    pub fn delete(&self, name: &str, key: &str) -> Result<Option<T>, InternalError<E>> {
+        let s = self.get_store(key)?;
+        s.delete(name)
+    }
+
+    pub fn get(&self, name: &str, key: &str) -> Result<World, InternalError<E>> {
+        match self.stores.get(key) {
+            Some(s) => s.get(name),
+            None => Err(InternalError::StoreNotFound),
+        }
+    }
+
+    pub fn list(&self, size: i64, key: &str) -> Result<Vec<World>, InternalError<E>> {
+        match self.stores.get(key) {
+            Some(s) => s.list(size),
+            None => Err(InternalError::StoreNotFound),
+        }
+    }
+
+    fn get_store(
+        &self,
+        key: &str,
+    ) -> Result<&Box<dyn Store<T, InternalError<E>, F>>, InternalError<E>> {
+        self.stores.get(key).ok_or(InternalError::StoreNotFound)
+    }
+
+    pub fn move_to(&self, name: &str, src: &str, dst: &str) -> Result<Option<T>, InternalError<E>> {
+        let s = self.get_store(src)?;
+        let d = self.get_store(dst)?;
+
+        let t0 = s.begin()?;
+
+        let word: World;
+        match t0.get(name) {
+            Ok(w) => word = w,
+            Err(err) => {
+                t0.rollback().unwrap();
+                return Err(err);
+            }
+        }
+
+        let t1 = d.begin()?;
+
+        if let Err(err) = t1.insert(&word) {
+            t0.rollback().unwrap();
+            t1.rollback().unwrap();
+            return Err(err);
+        }
+
+        if let Err(err) = t0.delete(&word.name) {
+            t0.rollback().unwrap();
+            t1.rollback().unwrap();
+            return Err(err);
+        }
+
+        // if the commit for insert error, recover before this func
+        if let Err(err) = t1.commit() {
+            t0.rollback().unwrap();
+            return Err(err);
+        }
+
+        // if the commit for delete error, the word will occur in two lirabry
+        if let Err(err) = t0.commit() {
+            return Err(err);
+        }
+
+        Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::db::sqlite::store;
+    extern crate sqlite;
+
+    fn new_word_library() -> WordLibrary<World, sqlite::Error, store::Tansaction> {
+        let k0 = "target/test.sql";
+        let k1 = "target/test1.sql";
+        let sql0 = store::Sqlite::new(k0.to_owned()).unwrap();
+        let sql1 = store::Sqlite::new(k1.to_owned()).unwrap();
+
+        let mut res = WordLibrary {
+            stores: HashMap::new(),
+        };
+
+        res.stores.insert(k0.to_owned(), Box::new(sql0));
+        res.stores.insert(k1.to_owned(), Box::new(sql1));
+        return res;
+    }
+
+    #[test]
+    fn clean_sqlite() {
+        let k0 = "target/test.sql";
+        let k1 = "target/test1.sql";
+        std::fs::remove_file(k0).unwrap();
+        std::fs::remove_file(k1).unwrap();
+    }
+
+    #[test]
+    fn test_move() {
+        let k0 = "target/test.sql";
+        let k1 = "target/test1.sql";
+        let wd = new_word_library();
+        let word = World {
+            name: String::from("test"),
+            message: String::from("test"),
+        };
+        wd.insert(&word, k0).unwrap();
+        wd.move_to(&word.name, k0, k1).unwrap();
+        wd.get(&word.name, k1).unwrap();
+        wd.list(1, k1).unwrap();
+        wd.update(&word, k1).unwrap();
+        wd.delete(&word.name, k1).unwrap();
+
+        clean_sqlite()
+    }
+}
